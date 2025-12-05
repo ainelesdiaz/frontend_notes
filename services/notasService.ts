27064e59cadb266b33bc17b48b211c02261fc307
ainelesdiaz <ainelesa07@gmail.com>
2025-12-05 15:55:35 +0000

    Funciona mierda

--- services/notasService.ts
+++ services/notasService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://apinotasdb-production.up.railway.app'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://apinotasdb-production.up.railway.app'
 
 export interface Nota {
   id?: number
